405ced4f5791735bd6a37f3687f18a5555c98176
sahawut <sahawut@gmail.com>
2022-03-19 10:41:52 -0400

    make a simple game

--- main.ts
+++ main.ts
@@ -1,29 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Orange))
-    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
-    strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Orange))
-    strip.show()
+    n = 0
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+    basic.showNumber(score)
 })
 input.onButtonPressed(Button.B, function () {
-    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
-    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Indigo))
-    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
-    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Orange))
-    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
-    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-    strip.show()
+    if (n == 14) {
+        score += 1
+    }
+    basic.showNumber(score)
 })
+let n = 0
+let score = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P1, 15, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.White))
-basic.showLeds(`
-    . # . # .
-    # # # # #
-    # # # # #
-    . # # # .
-    . . # . .
-    `)
+score = 0
+strip.showRainbow(1, 360)
+basic.showString("A")
+basic.forever(function () {
+    basic.pause(200)
+    strip.shift(1)
+    n += 1
+    strip.show()
+})
